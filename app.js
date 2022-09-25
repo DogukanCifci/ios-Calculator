a6671af544ac977ad33a0c4f0fa2ba29b75435b0
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-09-25 17:27:08 +0200

    =  added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let altYazi = document.querySelector(".current-display");
 let allBox = document.querySelector(".kutu");
 let icerik = document.querySelector(".kutu").value;
 let ilkSayi, sonuc;
+let sonSayi;
 let sayilar = [];
 let islemler = []; //sayilarin uzublugu 1den büyük oldugun da diye sart belirttigimiz icin 2. sayiyi girdigimizdeki islemi algiliyor. Bundan dolayi ilk islemi algilatmak icin listeye atayip ordan ilk islemi sectirtiyoruz. Daha sonra o ilk islemi gerceklestikten sonra silip ikinci islemi ilk islem olarak algilatiyoruz ve bu sirayla gidiyor
 document.querySelectorAll(".kutu").forEach((a) => {
@@ -16,6 +17,8 @@ document.querySelectorAll(".kutu").forEach((a) => {
       islemler.push(a.textContent);
       ilkSayi = Number(altYazi.textContent);
       sayilar.push(ilkSayi);
+      console.log(sayilar);
+      console.log(islemler);
 
       if (sayilar.length > 1) {
         if (islemler[0] == "+") sonuc = sayilar[0] + sayilar[1];
@@ -32,7 +35,20 @@ document.querySelectorAll(".kutu").forEach((a) => {
       ust_yazi.textContent = sayilar[0] + " " + a.textContent + " ";
       altYazi.textContent = "";
     }
+
+    if (a.textContent == "=") {
+      sonSayi = Number(altYazi.textContent);
+      if (islemler[0] == "+") sonuc = sayilar[0] + sonSayi;
+      else if (islemler[0] == "-") sonuc = sayilar[0] - sonSayi;
+      else if (islemler[0] == "x") sonuc = sayilar[0] * sonSayi;
+      else if (islemler[0] == "÷") sonuc = sayilar[0] / sonSayi;
+
+      ust_yazi.textContent = " ";
+      altYazi.textContent = sonuc;
+      console.log("Esittir sonrasi", sayilar);
+      console.log("Esittir sonrasi", islemler);
+      islemler.pop();
+      sayilar.pop();
+    }
   };
 });
-
-console.log(10 / 5);
